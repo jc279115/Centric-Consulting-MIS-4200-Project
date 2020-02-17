3112fc6a047fb09f9242dae38a9b3e366e8ce11c
Campbell <jc279115@ohio.edu>
2020-02-17 10:20:39 -0500

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Centric_Consulting_MIS_4200_Project.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Centric_Consulting_MIS_4200_Project.Startup))]
+namespace Centric_Consulting_MIS_4200_Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
